228d2c9afae51f94463438ce2a5408e567fb809f
Clark Drishinski <cdrishinski@gmail.com>
2018-05-01 23:27:09 -0600

    creating random option generator function

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var scissors = document.getElementById("scissors");
 var playerScore = 0;
 var computerScore = 0;
 
-function computerPlay () {
-
+function computerPlay (rock, paper, scissors) {
+  let options = [rock, paper,scissors];
+  let randomChoice = options[Math.floor(Math.random() * values.length)];
+  alert(randomChoice);
 }
+
+computerPlay();
+
+
+
+
+
+
+
+
+
+
