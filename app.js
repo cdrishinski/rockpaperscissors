c7cd7e9c5a896bc339c0ff39a1e13c4684779385
Clark Drishinski <cdrishinski@gmail.com>
2018-05-01 23:19:11 -0600

    creating document.getelemtnbyid linked to buttons

---
+++ app.js
@@ -0,0 +1,9 @@
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
+var playerScore = 0;
+var computerScore = 0;
+
+function computerPlay () {
+
+}
